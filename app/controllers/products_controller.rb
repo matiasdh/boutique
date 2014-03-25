6d863ded26b942365b7011cbc7787a63ef46c0e6
Matías de Horta <matias.dh@gmail.com>
2014-03-25 00:40:53 -0300

    translations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.all.order("position ASC")
   end
 
 end
